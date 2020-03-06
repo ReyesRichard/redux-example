66697d16483ecb22f34b5a9a6691782ffd19fa3c
Ricardo Reyes Torres <reyesrict@globalhitss.com>
2020-03-06 17:16:58 -0600

    Add eviroment variables and add value title in SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 function SEO(props) {
     return (
         <Helmet>
-            <title>{props.title}</title>
+            <title>{props.title ? `${props.title} | `: ''} {process.env.REACT_APP_TITLE_SITE}</title>
             <meta name="description" content={props.description} />
             <link rel='canonical' href={props.canonical} />
         </Helmet>
